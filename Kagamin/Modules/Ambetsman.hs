114f1326f62b2f3fccda5adbafa8f80889f72f72
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2016-06-16 14:58:04 +0200

    ambetsman added!

--- Kagamin/Modules/Ambetsman.hs
+++ Kagamin/Modules/Ambetsman.hs
@@ -4,42 +4,57 @@ module Kagamin.Modules.Ambetsman where
 
 import Kagamin.Modules
 import qualified Data.Text as T
-import Kagamin.TextUtils
 import Web.Slack.Message
-import Text.Read
-import System.Random
+import System.Random (randomRIO)
 import Control.Monad.State (MonadIO (..))
-import Data.Maybe (catMaybes)
-import KagaInfo (kagaID)
+import Data.Maybe (fromMaybe)
 import Data.Aeson
-import Data.Vector
-import System.Random 
+import Data.Default 
+import qualified Data.ByteString.Lazy as BL (readFile)
+import qualified Data.Vector as V
+import Control.Concurrent.MVar
 
-newtype Ambetsman = Ambetsman (Vector Text, Vector Text)
+-- | Ambetsman type with fromJSON instance
+newtype Ambetsman = Ambetsman (V.Vector T.Text, V.Vector T.Text)
 
 instance FromJSON Ambetsman where
   parseJSON (Object v) = Ambetsman <$> do
     job    <- v .: "job"
     surjob <- v .: "surjob"
     return (job, surjob)
+  parseJSON _ = fail "Badly formatted json :("
 
-loadAmbetsman = undefined 
+instance Default Ambetsman where
+  def = Ambetsman (V.fromList ["idiot"], V.fromList [""])
 
-jobname :: Ambetsman -> IO Text
-jobname (Ambetsman (job, surjob)) = do
-  prefix <- (job !)    <$> randomRIO (0, length job)
-  suffix <- (surjob !) <$> randomRIO (0, length surjob)
+-- | Loads the ambetsman
+loadAmbetsman :: MVar Ambetsman -> FilePath -> IO () 
+loadAmbetsman ambetsman dir = do
+  oldAmbetsman <- takeMVar ambetsman
+  ambets <- decode <$> BL.readFile (dir ++ "/assets/ambetsman.json")
+  putMVar ambetsman $ fromMaybe oldAmbetsman ambets
+
+-- | Generate a job 
+jobname :: MVar Ambetsman -> IO T.Text
+jobname ambetsman = do
+  Ambetsman (job, surjob) <- readMVar ambetsman 
+  prefix <- (job V.!)    <$> randomRIO (0, length job - 1)
+  suffix <- (surjob V.!) <$> randomRIO (0, length surjob - 1)
   return $ prefix `T.append` suffix
 
-ambetsman :: IO Kagamodule
-ambetsman = do
-  ambetsmanDB <- loadAmbetsman
-  return $ defaultModule {kagaMsgHook = handleKagaMsg ambetsmanDB }
+-- | Kagamin Ambetsman Module 
+kagaAmbetsman :: IO KagaModule
+kagaAmbetsman = do
+  ambetsman <- newMVar def
+  return $ defaultModule {kagaMsgHook = handleKagaMsg ambetsman,
+                          kagaLoadHook = loadAmbetsman ambetsman}
 
-handleKagaMsg :: Ambetsman -> MsgHook
+-- | Handle messages 
+handleKagaMsg :: MVar Ambetsman -> MsgHook
 handleKagaMsg ambetsman cid _from msg
   | "yrke" `T.isInfixOf` msg = do
-      jn <- jobname ambetsman
+      jn <- liftIO $ jobname ambetsman
       sendMessage cid $ T.concat ["Du borde jobba som ", jn]
+      return Next
   | otherwise = return Next
-      
+
